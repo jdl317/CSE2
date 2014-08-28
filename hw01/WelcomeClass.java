e8312f68bb8581e3a810cf45caa5ca81d5feaa77
jdl317 <jdl317@lehigh.edu>
2014-08-28 00:43:56 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Jeff Litterst
+//August 27, 2014
+//Welcome Class Homework Assignment
+
+// Define a class
+public class WelcomeClass{
+    
+    // Add main method
+    public static void main(String[] args) {
+        
+        // Print statements with designs around them
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-J--D--L--3--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Hello! I'm Jeff and I live on Lake Winnipesaukee in Moultonborough, NH.  I spent this summer working at a restaurant as a waiter.");
+        
+    }
+}
